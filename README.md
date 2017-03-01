4c63ae3c225d9e8b26383f354e4e1b4ec3aeab58
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2017-03-01 14:56:39 +0100

    Pandas Data Package Reader

---
+++ README.md
@@ -0,0 +1,23 @@
+# pandas-datapackage-reader
+
+Easy loading of tabular data from [Data Packages](http://frictionlessdata.io/data-packages/) into Pandas DataFrames.
+
+## Installation
+
+    pip install pandas-datapackage-reader
+
+## Usage
+
+```python
+from pandas_datapackage_reader import read_datapackage
+
+# From GitHub repository
+country_codes = read_datapackage("https://github.com/datasets/country-codes")
+
+# From local directory
+country_codes = read_datapackage("country-codes")
+```
+
+## License
+
+BSD-2-Clause, see [LICENSE]
---
+++ pandas_datapackage_reader/__init__.py
@@ -0,0 +1,105 @@
+# encoding: UTF-8
+
+import json
+import os
+
+import requests
+import pandas as pd
+
+
+def read_datapackage(url_or_path, resource_name=None):
+    """
+    Read tabular CSV files from Data Packages into DataFrames.
+
+    Parameters:
+    -----------
+    path_or_url: string
+        Local path or URL of a Data Package. For GitHub URLs the repository can
+        be used.
+    resource_name: string or list of strings
+        Name or names of resources to read. Lists of strings are used to
+        request multiple resources.
+
+    Notes:
+    ------
+    Columns of type "integer" with missing values are converted to "object" as
+    integer columns in Pandas do not support missing values.
+
+    Returns
+    -------
+    data_frames : DataFrame or Dict of DataFrames
+        DataFrame(s) of the passed in Data Package. See notes in resource_name
+        argument for more information on when a Dict of Dataframes is returned.
+
+    """
+    if url_or_path.startswith("https://github.com/"):
+        url_or_path = "https://raw.githubusercontent.com/" + \
+                       url_or_path.split("https://github.com/")[1] +\
+                       "/master/datapackage.json"
+    elif not url_or_path.endswith("datapackage.json"):
+        url_or_path = os.path.join(url_or_path, "datapackage.json")
+
+    if url_or_path.startswith("http"):
+        r = requests.get(url_or_path)
+        if r.status_code == 200:
+            metadata = json.loads(r.text)
+    else:
+        print(url_or_path)
+        metadata = json.load(open(url_or_path, "r"))
+
+    if type(resource_name) is str:
+        resource_name = [resource_name]
+
+    resources = [resource for resource in metadata["resources"]]
+    if resource_name is not None:
+        resources = [resource for resource in resources
+                     if resource["name"] in resource_name]
+
+    data_frames = {}
+
+    for idx, resource in enumerate(resources):
+        if "name" in resource.keys():
+            name = resource["name"]
+        else:
+            name = str(idx)
+
+        index_col = None
+        converters = {}
+        parse_dates = []
+
+        int_columns = []
+
+        csv_path = url_or_path.replace("datapackage.json", resource["path"])
+        if not csv_path.endswith(".csv"):
+            continue
+
+        if "primaryKey" in resource["schema"]:
+            index_col = resource["schema"]["primaryKey"]
+
+        for column in resource["schema"]["fields"]:
+            if column["type"] == "integer" and (index_col and column["name"]
+                                                not in index_col):
+                int_columns.append(column["name"])
+            elif column["type"] == "date":
+                parse_dates.append(column["name"])
+
+        if len(parse_dates) == 0:
+            parse_dates = None
+
+        df = pd.read_csv(
+            csv_path,
+            index_col=index_col,
+            converters=converters,
+            parse_dates=parse_dates
+        )
+        # Convert integer columns with missing values to type 'object'
+        for int_col in int_columns:
+            if df[int_col].isnull().sum() > 0:
+                df[int_col] = df[int_col].astype(object)
+
+        data_frames[name] = df
+
+    if len(list(data_frames.values())) == 1:
+        return list(data_frames.values())[0]
+    else:
+        return data_frames
