f5e09fd6782828f30f39037ed496b40bf7cee9fb
Robert Gieseke <robert.gieseke@pik-potsdam.de>
2019-01-30 11:11:21 +0100

    Fix setup.py

--- setup.py
+++ setup.py
@@ -58,9 +58,8 @@ setup(
         'Development Status :: 4 - Beta',
         "Intended Audience :: Developers",
         "Operating System :: OS Independent",
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     keywords=['data-package'],
     cmdclass=cmdclass,
